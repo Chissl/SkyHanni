d23e6bc276f7e5fb5ddf603164972a2994edd4e9
Chiss5618 <78828070+Chiss5618@users.noreply.github.com>
2025-09-10 10:07:12 -0500

    fix merge

--- src/main/java/at/hannibal2/skyhanni/utils/json/SkyHanniTypeAdapters.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/json/SkyHanniTypeAdapters.kt
@@ -77,8 +77,6 @@ object SkyHanniTypeAdapters {
         }
     }
 
-    val ELITE_LEADERBOARD_TYPE: TypeAdapter<EliteLeaderboardType> = EliteLeaderboardTypeAdapter()
-
     val STOPWATCH: TypeAdapter<Stopwatch> = SimpleStringTypeAdapter(
         { this.getDuration().inWholeMilliseconds.toString() },
         { this.toLongOrNull()?.milliseconds?.let { Stopwatch(it) } ?: error("Could not parse Stopwatch duration from '$this'") },
