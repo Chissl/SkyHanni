2bb2b2cca28f346735d8b475d38136e198a9329d
Chiss5618 <78828070+Chiss5618@users.noreply.github.com>
2025-09-18 19:05:02 -0500

    import trackerdata from uptimeV2

--- src/main/java/at/hannibal2/skyhanni/utils/tracker/TrackerData.kt
+++ src/main/java/at/hannibal2/skyhanni/utils/tracker/TrackerData.kt
@@ -7,35 +7,19 @@ import kotlin.time.Duration.Companion.seconds
 
 abstract class TrackerData {
     @Expose
-    internal open var sessionUptime: Map<SessionUptime, Stopwatch> = mapOf(
-        Pair(SessionUptime.Normal(NormalSession.NORMAL), Stopwatch())
-    )
+    private val sessionUptime: Map<SessionUptime.Normal, Stopwatch> = mapOf(SessionUptime.Normal(NormalSession.NORMAL) to Stopwatch())
 
-    private var _activeSession: SessionUptime? = null
-
-    // avoid initializing until we need it since subclass overrides sessionUptime
-    private var activeSession: SessionUptime?
-        get() {
-            if (_activeSession == null) {
-                _activeSession = sessionUptime.keys.firstOrNull()
-            }
-            return _activeSession
-        }
-        private set(value) {
-            _activeSession = value
-        }
+    private var activeSession: SessionUptime? = sessionUptime.keys.firstOrNull()
 
     fun getActiveStopwatch(): Stopwatch? = activeSession?.let { sessionUptime[it] }
 
     fun setActiveStopwatch(session: SessionUptime) {
-        require(sessionUptime.containsKey(session)) {
-            "Session $session not part of this tracker"
+        if (session != activeSession) {
+            val duration = sessionUptime[activeSession]?.pause(revertLap = true)
+            activeSession = session
+            sessionUptime[activeSession]?.add(duration ?: 0.seconds)
         }
-        if (session == activeSession) return
-        val duration = sessionUptime[activeSession]?.pause(revertLap = true)
-        activeSession = session
-        sessionUptime[activeSession]?.start()
-        sessionUptime[activeSession]?.add(duration ?: 0.seconds)
+        sessionUptime[activeSession]?.start(true)
     }
 
     open fun getTotalUptime(): Duration =
@@ -53,15 +37,6 @@ abstract class TrackerData {
     protected abstract fun resetData()
 }
 
-abstract class GardenTrackerData : TrackerData() {
-    @Expose
-    override var sessionUptime: Map<SessionUptime, Stopwatch> = mapOf(
-        Pair(SessionUptime.Garden(GardenSession.CROP), Stopwatch()),
-        Pair(SessionUptime.Garden(GardenSession.PEST), Stopwatch()),
-        Pair(SessionUptime.Garden(GardenSession.VISITOR), Stopwatch())
-    )
-}
-
 sealed class SessionUptime {
     data class Normal(val sessionType: NormalSession) : SessionUptime()
     data class Garden(val sessionType: GardenSession) : SessionUptime()
@@ -76,3 +51,5 @@ enum class GardenSession {
     VISITOR,
     CROP,
 }
+
+
