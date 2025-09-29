ddd6c360df06ec49fc189c033bcb3d35b0a47976
Chiss5618 <78828070+Chiss5618@users.noreply.github.com>
2025-09-28 21:35:27 -0500

    deteky

--- src/main/java/at/hannibal2/skyhanni/features/garden/tracker/VisitorDropTracker.kt
+++ src/main/java/at/hannibal2/skyhanni/features/garden/tracker/VisitorDropTracker.kt
@@ -45,7 +45,6 @@ import com.google.gson.annotations.Expose
 import java.util.EnumMap
 import java.util.regex.Pattern
 import kotlin.math.round
-import kotlin.time.Duration.Companion.seconds
 
 @SkyHanniModule
 object VisitorDropTracker : SkyHanniTimedBucketedItemTracker<VisitorRarity, VisitorDropTracker.BucketData>(
